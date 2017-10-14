9dad2e56b9d8508de544b0580c0c3d6011aca1b0
David Catuhe <david.catuhe@Live.fr>
2017-10-14 09:03:26 -0700

    First commit

---
+++ 3ds Max/BabylonExport.Entities/BabylonIAnimatable.cs
@@ -0,0 +1,14 @@
+﻿namespace BabylonExport.Entities
+{
+    public interface BabylonIAnimatable
+    {
+        BabylonAnimation[] animations { get; set; }
+        bool autoAnimate { get; set; }
+
+        int autoAnimateFrom { get; set; }
+
+        int autoAnimateTo { get; set; }
+
+        bool autoAnimateLoop { get; set; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+Babylon.js exporters
+====================
+
+Documentation for all exporters is available here: http://doc.babylonjs.com/exporters
+
+**Any questions?** Here is our official [forum](http://www.html5gamedevs.com/forum/16-babylonjs/) on www.html5gamedevs.com.
