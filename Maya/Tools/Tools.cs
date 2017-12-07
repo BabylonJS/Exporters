9ee8166118d508ebab49f4357cb596d63b98a92f
noalak <tambrun.fabrice@gmail.com>
2017-12-07 17:23:47 +0100

    Missing files

---
+++ Maya/Tools/Tools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Maya2Babylon
+{
+    static class Tools
+    {
+        public const float Epsilon = 0.001f;
+
+        public static bool IsAlmostEqualTo(this float[] current, float[] other, float epsilon)
+        {
+            if (current.Length != other.Length)
+            {
+                return false;
+            }
+
+            for (int index = 0; index < current.Length; index++)
+            {
+                if (Math.Abs(current[index] - other[index]) > epsilon)
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+
+        public static string toString<T>(this T[] array)
+        {
+            if (array == null)
+            {
+                return "";
+            }
+
+            var result = "[";
+            bool isFirst = true;
+            for (uint index = 0; index < array.Length; index++)
+            {
+                if (!isFirst)
+                {
+                    result += ",";
+                }
+                isFirst = false;
+                result += array[index];
+            }
+            return result + "]";
+        }
+    }
+}
