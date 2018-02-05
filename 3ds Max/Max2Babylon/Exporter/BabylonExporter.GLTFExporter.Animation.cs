62a4c01254801376caa6d5c36ac7a472a6a1f844
noalak <tambrun.fabrice@gmail.com>
2018-02-05 18:11:02 +0100

    Update conversion from left to right handed coordinate system
    Alter positions, rotations and normals at object level rather than adding a root node with negative scaling

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.GLTFExporter.Animation.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.GLTFExporter.Animation.cs
@@ -65,6 +65,18 @@ namespace Max2Babylon
                     foreach (var babylonAnimationKey in babylonAnimation.keys)
                     {
                         var outputValues = babylonAnimationKey.values;
+
+                        // Switch coordinate system at object level
+                        if (babylonAnimation.property == "position")
+                        {
+                            outputValues[2] *= -1;
+                        }
+                        else if (babylonAnimation.property == "rotationQuaternion")
+                        {
+                            outputValues[0] *= -1;
+                            outputValues[1] *= -1;
+                        }
+
                         // Store values as bytes
                         foreach (var outputValue in outputValues)
                         {
@@ -153,6 +165,12 @@ namespace Max2Babylon
                     var scaleBabylon = new BabylonVector3();
                     matrix.decompose(scaleBabylon, rotationQuatBabylon, translationBabylon);
 
+                    translationBabylon.Z *= -1;
+                    BabylonVector3 rotationVector3 = rotationQuatBabylon.toEulerAngles();
+                    rotationVector3.X *= -1;
+                    rotationVector3.Y *= -1;
+                    rotationQuatBabylon = rotationVector3.toQuaternion();
+
                     var outputValuesByPath = new Dictionary<string, float[]>();
                     outputValuesByPath.Add("translation", translationBabylon.ToArray());
                     outputValuesByPath.Add("rotation", rotationQuatBabylon.ToArray());
@@ -163,6 +181,7 @@ namespace Max2Babylon
                     {
                         var accessorOutput = accessorOutputByPath[path];
                         var outputValues = outputValuesByPath[path];
+
                         foreach (var outputValue in outputValues)
                         {
                             accessorOutput.bytesList.AddRange(BitConverter.GetBytes(outputValue));
