54c5d42d10ce93826d38722c687394d14bd981ab
Selmar Kok <skok@asobostudio.com>
2018-03-19 15:24:05 +0100

    support for extensions

---
+++ SharedProjects/GltfExport.Entities/GLTFExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace GLTFExport.Entities
+{
+    [CollectionDataContract]
+    public class GLTFExtensions : Dictionary<string, object>
+    {
+
+    }
+}
