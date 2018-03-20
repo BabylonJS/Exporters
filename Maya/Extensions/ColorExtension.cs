4f2822080e7d132b99604d079a5376d38b95a906
noalak <tambrun.fabrice@gmail.com>
2018-03-20 17:08:11 +0100

    Fix emissive for standard materials

---
+++ Maya/Extensions/ColorExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+namespace Maya2Babylon
+{
+    static class ColorExtension
+    {
+        /// <summary>
+        /// [r,g,b,a]
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static float[] toArray(this Color color)
+        {
+            return new float[] { color.R, color.G, color.B, color.A };
+        }
+
+        /// <summary>
+        /// [r,g,b]
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static float[] toArrayRGB(this Color color)
+        {
+            return new float[] { color.R, color.G, color.B };
+        }
+
+        public static string toString(this Color color)
+        {
+            return color == null ? "" : color.toArray().toString();
+        }
+    }
+}
