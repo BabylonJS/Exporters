27c7f5d95ac4677583876732987576d8b964d955
noalak <tambrun.fabrice@gmail.com>
2018-06-05 16:45:40 +0200

    Update dlls

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -30,7 +30,7 @@ namespace Max2Babylon
 
         private bool isBabylonExported;
 
-        private string exporterVersion = "1.2.0";
+        private string exporterVersion = "1.2.1";
 
         void ReportProgressChanged(int progress)
         {
