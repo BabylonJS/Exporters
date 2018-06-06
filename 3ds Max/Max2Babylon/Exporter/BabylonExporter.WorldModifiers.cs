3c53b58b840f3ce259e20fd03c334b24cbbab085
noalak <tambrun.fabrice@gmail.com>
2018-06-06 12:03:33 +0200

    Add German detection for Hair and Fur modifier

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.WorldModifiers.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.WorldModifiers.cs
@@ -17,13 +17,16 @@ namespace Max2Babylon
             var derivedObject = meshNode.MaxNode.WSMDerivedObject;
             if (derivedObject == null)
             {
-                RaiseMessage("derivedOvject is null", 2);
+                RaiseMessage("WSMDerivedObject is null", 2);
             }
             else
             {
                 foreach (var modifier in derivedObject.Modifiers)
                 {
-                    if (modifier.Name == "Hair and Fur" || modifier.Name == "Chevelure et Pelage")
+                    // TODO - Find another way to detect if modifier is a HairAndFur
+                    if (modifier.Name == "Hair and Fur" || // English
+                        modifier.Name == "Haar und Fell" || // German
+                        modifier.Name == "Chevelure et Pelage") // French
                     {
                         var babylonFurMaterial = ExportFurModifier(modifier, babylonMesh.name, babylonScene);
                         babylonScene.MaterialsList.Add(babylonFurMaterial);
@@ -31,7 +34,7 @@ namespace Max2Babylon
                     }
                     else
                     {
-                        RaiseWarning("Modifier or Language" + modifier.Name + " is not supported");
+                        RaiseWarning("Modifier or Language '" + modifier.Name + "' is not supported", 2);
                     }
                 }
             }
@@ -39,6 +42,7 @@ namespace Max2Babylon
 
         private BabylonFurMaterial ExportFurModifier(IModifier modifier, String sourceMeshName, BabylonScene babylonScene)
         {
+            RaiseMessage("Export Fur Modifier", 2);
             var paramBlock = modifier.GetParamBlock(0);
             
             // 3dsMax "Cut Length" is in percentages - "100%" will be "20" babylon spacing 
@@ -56,7 +60,7 @@ namespace Max2Babylon
 
             if (paramBlock.GetTexmap(MAPS_PARAM_ID, 0, 11) != null)
             {
-                RaiseWarning("tip texture is not supported - use root texture instead");
+                RaiseWarning("Tip texture is not supported. Use root texture instead", 2);
             }
 
             BabylonTexture diffuseTexture = null;
