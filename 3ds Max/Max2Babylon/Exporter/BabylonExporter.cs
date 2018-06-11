1a345a6684a1feaf58bf43bf0e5251a7b0310edc
Selmar Kok <skok@asobostudio.com>
2018-06-11 11:52:52 +0200

    fix invalid rank exception; print a warning message for it instead

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -86,6 +86,9 @@ namespace Max2Babylon
         
         public void Export(ExportParameters exportParameters)
         {
+            RaiseMessage("testestestest", 5);
+            RaiseMessage("testestestest", -5);
+
             // Check input text is valid
             var scaleFactorFloat = 1.0f;
             string scaleFactor = exportParameters.scaleFactor;
