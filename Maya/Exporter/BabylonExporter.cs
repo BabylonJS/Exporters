079fd7e3f92831b6fb1cc4f63b98c0eed8c5bc31
Rémi Louvat <remi.louvat@hotmail.com>
2018-06-21 14:09:45 +0200

    Maya: update version

--- Maya/Exporter/BabylonExporter.cs
+++ Maya/Exporter/BabylonExporter.cs
@@ -35,7 +35,7 @@ namespace Maya2Babylon
         /// </summary>
         private static List<string> defaultCameraNames = new List<string>(new string[] { "persp", "top", "front", "side" });
 
-        private string exporterVersion = "1.2.0";
+        private string exporterVersion = "1.2.1";
 
         public void Export(string outputDirectory, string outputFileName, string outputFormat, bool generateManifest,
                             bool onlySelected, bool autoSaveMayaFile, bool exportHiddenObjects, bool copyTexturesToOutput,
