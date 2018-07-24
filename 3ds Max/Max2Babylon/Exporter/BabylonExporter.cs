1994d0233cea8bf074d1b10e09a14d1916273b08
Rémi Louvat <remi.louvat@hotmail.com>
2018-07-24 14:30:15 +0200

    3DS Max: update version

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -30,7 +30,7 @@ namespace Max2Babylon
 
         private bool isBabylonExported, isGltfExported;
 
-        private string exporterVersion = "1.2.19";
+        private string exporterVersion = "1.2.20";
 
         void ReportProgressChanged(int progress)
         {
