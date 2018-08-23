c0ec20b87e409219a476d692882b8e7f79bce229
noalak <tambrun.fabrice@gmail.com>
2018-08-23 16:31:57 +0200

    Do not copy file to same path

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -30,7 +30,7 @@ namespace Max2Babylon
 
         private bool isBabylonExported, isGltfExported;
 
-        private string exporterVersion = "1.2.27";
+        private string exporterVersion = "1.2.28";
 
         void ReportProgressChanged(int progress)
         {
