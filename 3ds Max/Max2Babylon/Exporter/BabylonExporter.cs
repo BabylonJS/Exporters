7648c318712084f07676ff44c7efc44634dc52f7
Rémi Louvat <remi.louvat@hotmail.com>
2018-09-07 12:00:59 +0200

    [3DS Max & Maya] update version and samples

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -32,7 +32,7 @@ namespace Max2Babylon
         private bool optimizeAnimations;
         private bool exportNonAnimated;
 
-        private string exporterVersion = "1.2.33";
+        private string exporterVersion = "1.2.34";
 
         void ReportProgressChanged(int progress)
         {
