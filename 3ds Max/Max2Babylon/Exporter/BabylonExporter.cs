eefe7243a14de2d975d256cf509934abc388f206
Rémi Louvat <remi.louvat@hotmail.com>
2018-09-11 10:14:22 +0200

    [3DS Max] update version

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -32,7 +32,7 @@ namespace Max2Babylon
         private bool optimizeAnimations;
         private bool exportNonAnimated;
 
-        private string exporterVersion = "1.2.37";
+        private string exporterVersion = "1.2.38";
 
         void ReportProgressChanged(int progress)
         {
