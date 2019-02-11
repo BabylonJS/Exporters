dc81eaf5afc401d12ec48780e915e745b23ac758
Kacey Coley <kcoley@users.noreply.github.com>
2019-02-10 19:05:04 -0800

    Maya animation group same two key frames (#456)
    ## v1.2.29
    **Fixed bugs:**
    - Allow animation groups with two key frames of the same value to export

--- Maya/Exporter/BabylonExporter.cs
+++ Maya/Exporter/BabylonExporter.cs
@@ -41,7 +41,7 @@ namespace Maya2Babylon
         /// </summary>
         private static List<string> defaultCameraNames = new List<string>(new string[] { "persp", "top", "front", "side" });
 
-        public static string exporterVersion = "1.2.28";
+        public static string exporterVersion = "1.2.29";
 
         /// <summary>
         /// Export to file
