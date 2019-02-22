3fed9f8ea5a8aa4e6671a9f93123bbc9bbd27bd9
Kacey Coley <kaceylefty@gmail.com>
2019-02-21 20:44:00 -0800

    texture transform fix

--- 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
+++ 3ds Max/Max2Babylon/Exporter/BabylonExporter.cs
@@ -32,7 +32,7 @@ namespace Max2Babylon
         private bool optimizeAnimations;
         private bool exportNonAnimated;
 
-        public static string exporterVersion = "1.3.32";
+        public static string exporterVersion = "1.3.33";
 
         void ReportProgressChanged(int progress)
         {
