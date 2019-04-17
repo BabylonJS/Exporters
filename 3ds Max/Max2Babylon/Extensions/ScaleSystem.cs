0dd62fd522088c5ec8e09689036e704154eff55d
Luca Pierabella <lpierabella@asobostudio.com>
2019-04-17 14:31:09 +0200

    unit scale support added

---
+++ 3ds Max/Max2Babylon/Extensions/ScaleSystem.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max2Babylon.Extensions
+{
+    public enum ScaleUnitType
+    {
+        Inches,
+        Feet,
+        Miles,
+        Millimeters,
+        Centimeters,
+        Meters,
+        Kilometers
+    }
+}
