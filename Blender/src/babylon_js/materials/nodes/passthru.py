5512d89dc3fd04474130532040c173ae4f75d5e5
Palmer-JC <jeffrey.c.palmer@gmail.com>
2019-06-12 10:16:39 -0400

    Blender moved

--- Blender/src/babylon_js/materials/nodes/passthru.py
+++
@@ -1,8 +0,0 @@
-from .abstract import AbstractBJSNode
-
-#===============================================================================
-class PassThruBJSNode(AbstractBJSNode):
-    PASS_THRU_SHADERS = 'ShaderNodeMixShader ShaderNodeSeparateRGB'
-
-    def __init__(self, bpyNode, socketName):
-        super().__init__(bpyNode, socketName)
