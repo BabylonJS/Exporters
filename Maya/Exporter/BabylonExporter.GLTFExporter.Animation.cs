3028857e8d083c0976d570ed018f1bb2b013d55f
Nicholas Barlow <nickmaxx@gmail.com>
2019-06-21 18:16:46 -0700

    Fix for #549, for GLTF export add additional animation relevance check against entire animation channel

--- Maya/Exporter/BabylonExporter.GLTFExporter.Animation.cs
+++ Maya/Exporter/BabylonExporter.GLTFExporter.Animation.cs
@@ -158,11 +158,23 @@ namespace Maya2Babylon
                 {
                     // Filter animation keys to only keep frames between start and end
                     List<BabylonAnimationKey> keysInRangeFull = babylonAnimation.keysFull.FindAll(babylonAnimationKey => babylonAnimationKey.frame >= startFrame && babylonAnimationKey.frame <= endFrame);
-
+                    
                     // Optimization process always keeps first and last frames
                     OptimizeAnimations(keysInRangeFull, true);
+                    bool keysInRangeAreRelevant = IsAnimationKeysRelevant(keysInRangeFull, babylonAnimation.property);
 
-                    if (IsAnimationKeysRelevant(keysInRangeFull, babylonAnimation.property))
+                    // if we are baking the animation frames, then do a less efficient check against all frames in the scene for this animation channel
+                    if (!keysInRangeAreRelevant && _bakeAnimationFrames)
+                    {
+                        List<BabylonAnimationKey> optimizedKeysFull = new List<BabylonAnimationKey>(babylonAnimation.keysFull);
+                        OptimizeAnimations(optimizedKeysFull, true);
+                        keysInRangeAreRelevant = IsAnimationKeysRelevant(optimizedKeysFull, babylonAnimation.property);
+                    }
+
+                    // If we have any significant animation keys in this channel
+                    // (or if we are baking the animation track and have any significant animation keys in the whole track)
+                    // then add the optimized keys in range to the animation track
+                    if (keysInRangeAreRelevant)
                     {
                         // Override animation keys
                         babylonAnimation.keys = keysInRangeFull.ToArray();
