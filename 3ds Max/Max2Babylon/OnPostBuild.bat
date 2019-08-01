27002c3b3ba29eb198442b5f1d2883bcc0e39db0
elpie89 <pierabellaluca@gmail.com>
2019-08-01 14:39:53 +0200

    fix compile error

--- 3ds Max/Max2Babylon/OnPostBuild.bat
+++ 3ds Max/Max2Babylon/OnPostBuild.bat
@@ -14,13 +14,13 @@ ECHO "Exporter version is %exporter_version%"
 SET source_dir="%~dp0%exporter_version%\bin\%1"
 ECHO %source_dir%
 
-IF %1=="Debug" GOTO OnDebug
-IF %1=="Release" GOTO OnRelease
-
 IF "%max_location%"=="" (
 	ECHO 3DS Max %max_version% not installed. Skipping copy.
 	GOTO Close
 )
+
+IF %1=="Debug" GOTO OnDebug
+IF %1=="Release" GOTO OnRelease
 
 :OnDebug
 SET dest_dir="%max_location%bin\assemblies"
