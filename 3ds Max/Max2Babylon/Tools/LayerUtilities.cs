897801fde5abcc5e778ff13d4cd2891ae549dc69
elpie89 <pierabellaluca@gmail.com>
2019-08-23 10:50:31 +0200

    added 3ds Max .NET Assemblies to older export versions

--- 3ds Max/Max2Babylon/Tools/LayerUtilities.cs
+++ 3ds Max/Max2Babylon/Tools/LayerUtilities.cs
@@ -67,6 +67,9 @@ namespace Max2Babylon
         public static IEnumerable<T> ITabToIEnumerable<T>(ITab<T> tab)
         {
             for (int i = 0; i < tab.Count; i++)
+                #if MAX2015
+                yield return tab[(IntPtr)i];
+                #endif
                 yield return tab[i];
         }
     }
