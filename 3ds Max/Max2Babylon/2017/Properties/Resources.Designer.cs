1e89b0953ae405321202c8f18abbe4f64c2176a7
Guillaume Pelletier <guillaume.pelletier@dotvision.com>
2021-05-15 01:37:25 +0200

    Add max bump properties to 3dsMax (#957)
    * create BumbMap script
    
    * Add support for Normal map input infos

--- 3ds Max/Max2Babylon/2017/Properties/Resources.Designer.cs
+++ 3ds Max/Max2Babylon/2017/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace Max2Babylon.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] BUMP_MAP_CAT_DEF {
+            get {
+                object obj = ResourceManager.GetObject("BUMP_MAP_CAT_DEF", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap MaxExporter {
