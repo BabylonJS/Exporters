852af2f14555944938bb87a8b3b8ea502d4212b7
Josef Wienerroither <spacefrog@frogsinspace.at>
2024-10-23 13:55:12 +0200

    Add menu creation callback maxscript to resources

--- 3ds Max/Max2Babylon/2025/Properties/Resources.Designer.cs
+++ 3ds Max/Max2Babylon/2025/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace Max2Babylon.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] CreateBabylonMenus {
+            get {
+                object obj = ResourceManager.GetObject("CreateBabylonMenus", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap MaxExporter {
